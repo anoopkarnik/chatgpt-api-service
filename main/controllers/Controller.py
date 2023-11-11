cfe352a6202f98c90f2eef351893550e8cce010d
anoopkarnik <danoopkarnik@gmail.com>
2023-11-12 00:09:03 +0530

    Added audio capabilities to this client

--- main/controllers/Controller.py
+++ main/controllers/Controller.py
@@ -1,5 +1,9 @@
 from flask import Blueprint, request, jsonify
 from main.services.assistants import get_reply
+from main.services.audio import create_text_to_speech,create_speech_to_text,translate_speech_to_text
+import logging
+
+logger = logging.getLogger(__name__)
 
 payload_controller = Blueprint("payload_controller",__name__)
 
@@ -12,3 +16,24 @@ def chat_with_assistant():
 	message_body = request.json
 	result = get_reply(message_body)
 	return jsonify(result)
+
+@payload_controller.route("/text_to_speech",methods=["POST"])
+def text_to_speech():
+	logger.info(f'Request received: {request.json}')
+	message_body = request.json
+	create_text_to_speech(message_body)
+	return jsonify({'result':"success"})
+
+@payload_controller.route("/speech_to_text",methods=["POST"])
+def speech_to_text():
+	logger.info(f'Request received: {request.json}')
+	message_body = request.json
+	transcript = create_speech_to_text(message_body)
+	return jsonify(transcript)
+
+@payload_controller.route("/translate",methods=["POST"])
+def translate():
+	logger.info(f'Request received: {request.json}')
+	message_body = request.json
+	transcript = translate_speech_to_text(message_body)
+	return jsonify(transcript)
---
+++ main/services/audio.py
@@ -0,0 +1,57 @@
+from openai import OpenAI
+import os
+import logging
+import requests
+import time
+import json
+import pandas as pd
+from flask_sqlalchemy import SQLAlchemy
+from pydub import AudioSegment
+from pydub.playback import play
+import pyaudio
+import uuid
+
+logger = logging.getLogger(__name__)
+
+def create_text_to_speech(message_body):
+    # client = OpenAI(api_key='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')
+    logger.info(f'OpenAI client created')
+    client = OpenAI(api_key=os.environ.get('OPENAI_API_KEY'))
+    logger.info(f'OpenAI client created')
+    response = client.audio.speech.create(
+        model='tts-1',
+        voice='nova',
+        input=message_body['message'],
+    )
+    logger.info(f'Speech created: {response}')
+    if 'output_path' in message_body:
+        output_path = message_body['output_path']
+    else:
+        output_path = 'data'
+    if 'file_name' in message_body:
+        file_name = message_body['file_name']
+    else:
+        file_name = f'{uuid.uuid4()}.mp3'
+    response.stream_to_file(os.path.join(output_path,file_name))
+    audio = AudioSegment.from_mp3(os.path.join(output_path,file_name))
+    play(audio)
+
+def create_speech_to_text(message_body):
+    client = OpenAI(api_key=os.environ.get('OPENAI_API_KEY'))
+    logger.info(f'OpenAI client created')
+    audio_file = open(message_body['path'], 'rb')
+    transcript = client.audio.transcriptions.create(
+        model='whisper-1',
+        file=audio_file,
+    )
+    return json.loads(transcript.json())
+
+def translate_speech_to_text(message_body):
+    client = OpenAI(api_key=os.environ.get('OPENAI_API_KEY'))
+    logger.info(f'OpenAI client created')
+    audio_file = open(message_body['path'], 'rb')
+    transcript = client.audio.translations.create(
+        model='whisper-1',
+        file=audio_file,
+    )
+    return json.loads(transcript.json())
